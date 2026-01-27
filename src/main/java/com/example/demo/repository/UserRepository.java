0001313b0f47f98f28e2d7a30bdbc455e48dee7e
Gam1nerie <lemmy12347@gmail.com>
2026-01-27 13:19:42 +0100

    java util list :steamhappy:

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+import java.util.List; // AJOUTE CETTE LIGNE
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email); // Pour login / vérification
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    
+    // La méthode de recherche
     List<User> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
 }
